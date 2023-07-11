36ea1a3f491afa171f91f9e337b6b71990070eda
Andres <andrese-rios@utp.edu.co>
2023-07-11 11:42:19 -0500

    swip a list of classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList()
@@ -18,6 +19,12 @@ class _QuoteListState extends State<QuoteList> {
     'No malgastes tu tiempo, pues de esa materia está formada la vida. Benjamin Franklin.',
     'La buena conciencia es la mejor almohada para dormir. Sócrates.'
     ];
+  
+  List<Quote> qList = [Quote(text: 'Hay alguien tan inteligente que aprende de la experiencia de los demás', author: 'Voltaire'),
+                       Quote(text: 'No malgastes tu tiempo, pues de esa materia está formada la vida', author: 'Benjamin Franklin'),
+                       Quote(text: 'La buena conciencia es la mejor almohada para dormir', author: 'Sócrates')
+                      ];
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +37,12 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((q) => Text(q)).toList(),
+        children: qList.map((q) => Row(
+          children: [Text(q.text),
+                     SizedBox(width: 10), 
+                     Text(q.author)],
+                                         
+        )).toList(),
       ),
     );
   }
